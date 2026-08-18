5be89782696fb1058106cdd918ab40374bf4e2f1
Vikash Mundakar <m.vikash1630@gmail.com>
2026-08-18 21:09:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,37 +1,30 @@
 class Solution {
   public:
-    int aggressiveCows(vector<int> &stalls, int k) {
+    int aggressiveCows(vector<int> &arr, int k) {
         // code here
-        if (k > stalls.size()) return 0;
-        sort(stalls.begin(),stalls.end());
-        int left = 0;
-        int n = stalls.size();
-        int right = stalls[n - 1];
-        int ans = 0;
-        while (left <= right) {
-            int mid = left + (right - left)/2;
-            if (isPossible(stalls,k,mid)) {
-                // Min distance with mid is possible so to make it maximize try something larger than this mid
-                ans = mid;
-                left = mid + 1;
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        int low = arr[1] - arr[1];
+        int high = arr[n - 1] - arr[0];
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            int cows = 1;
+            int prevCowInd = 0;
+            for (int i = 1;i<n;i++) {
+                if (arr[i] - arr[prevCowInd] >= mid) {
+                    prevCowInd = i;
+                    cows++;
+                }
             }
-            else right = mid - 1;
+            if (cows >= k) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
         }
         return ans;
-    }
-    
-    bool isPossible(vector<int>& stalls,int k,int dis) {
-        int n = stalls.size();
-        int cnt = 1; // Cow at 0th index
-        int nextPossiblePos = stalls[0] + dis;
-        for (int i = 1;i<n;i++) {
-            if (stalls[i] >= nextPossiblePos) {
-                // Placed Second Cow
-                cnt++;
-                nextPossiblePos = stalls[i] + dis;
-            }
-            if (cnt == k) return true;
-        }
-        return false;
     }
 };
